5b2b3558581c660edde80d6f51698630e3c61b25
Joseph Sgrignoli <JSgrignoli@hotmail.com>
2022-07-21 19:38:37 -0400

    cleaned up playing board interfaced and made larger sprites for the playing board

--- blockpuzzle.js
+++ blockpuzzle.js
@@ -7,26 +7,22 @@ class BlockPuzzle extends Phaser.Scene {
     }
 
 preload() {
-    this.load.spritesheet('block', 'filledblock.png', { frameWidth: 14, frameHeight: 14 })
+    this.load.spritesheet('block', 'block.png', { frameWidth: 20, frameHeight: 20 })
    
 
 }//preload closing bracket
 
 create() {
-    this.add.image(150, 150, 'block')
-    blocks = this.add.group();
-
-    block = this.add.rectangle(50, 50, 15, 15, 0xffffff)
-    blocks.add(block);
-    
-    // block.on('pointerup', () => {
-    //     block.setAlpha(.4)
-    // })
-
     this.createBoard();
 
-    this.input.on('gameobjectdown', function (pointer, gameObject) {
-        gameObject.setAlpha(.4)
+    this.input.on('gameobjectup', function (pointer, gameObject) {
+        if (gameObject.alpha === 1){
+            gameObject.setAlpha(.4)
+            console.log(gameObject)
+        }else {
+            gameObject.setAlpha(1)
+        }
+        
     }, this)
     
 
@@ -39,9 +35,9 @@ update() {
     
 }//update closing bracket
 createBoard() {
-    for(let row=0; row < 20; row+=1){
-        for(let col=0; col < 30; col+=1){
-            block = this.add.sprite(100+row*16, 200+col*16, 'block').setInteractive();
+    for(let row=0; row < 15; row+=1){
+        for(let col=0; col < 20; col+=1){
+            block = this.add.sprite(50+row*25, 50+col*25, 'block').setInteractive();
         }
     }
 }
@@ -52,8 +48,8 @@ createBoard() {
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 900,
+    width: 450,
+    height: 575,
     backgroundColor: "#000000",
     scene: [ BlockPuzzle ]
 };
