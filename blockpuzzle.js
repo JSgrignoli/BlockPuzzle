41c153b8f566d7975a83ee50941509f43e37391d
Joseph Sgrignoli <JSgrignoli@hotmail.com>
2022-07-24 10:49:42 -0400

    added numbers to blocks to help identify sprites for logic puzzle logie

--- blockpuzzle.js
+++ blockpuzzle.js
@@ -1,5 +1,8 @@
-let blocks;
+let blocks = [];
 let block = [];
+let index = 0;
+let index2 = 0;
+let finder = 0;
 
 class BlockPuzzle extends Phaser.Scene {
     constructor() {
@@ -13,17 +16,36 @@ preload() {
 }//preload closing bracket
 
 create() {
+    let style = { font: '16px', fill: '0xff0000' };
+    this.add.text(175, 10, 'Block Puzzle', { fill: '#ffff' })
     this.createBoard();
+    
 
-    this.input.on('gameobjectup', function (pointer, gameObject) {
-        if (gameObject.alpha === 1){
-            gameObject.setAlpha(.4)
-            console.log(gameObject)
+    blocks[299].setAlpha(.4);
+
+    this.input.on('gameobjectover', function (pointer, gameObject) {
+        gameObject.setTint('0xff0000')
+    })
+    this.input.on('gameobjectout', function (pointer, gameObject) {
+        gameObject.clearTint();
+    })
+
+
+    this.input.on('gameobjectdown', function (pointer, gameObject) {
+        if(gameObject.index % 20 === 0 && blocks[gameObject.index].alpha === 1){
+            blocks[gameObject.index].setAlpha(.4)
+    
         }else {
-            gameObject.setAlpha(1)
+            blocks[gameObject.index].setAlpha(1);
         }
         
-    }, this)
+    })
+    for(let num = 0; num < 15; num++) {
+        for(let ber = 0; ber < 20; ber++) {
+            this.add.text(40+num*25, 45+ber*25, `${finder}`, { font: '10px', fill: '#000' })
+            finder++
+    }
+}
     
 
 }//create closing bracket
@@ -38,7 +60,17 @@ createBoard() {
     for(let row=0; row < 15; row+=1){
         for(let col=0; col < 20; col+=1){
             block = this.add.sprite(50+row*25, 50+col*25, 'block').setInteractive();
+            block.index = index;
+            index++
+            blocks.push(block)
         }
+    }
+}
+
+fill() {
+    if(gameObject.index % 20 === 0){
+        block[gameObject.index].setAlpha(.4)
+
     }
 }
 
