76ebd4d9760439b2dd8e2ac3b427dfdaa8520f97
Joseph Sgrignoli <JSgrignoli@hotmail.com>
2022-07-26 19:31:46 -0400

    added an easy mode

--- blockconfig.js
+++ blockconfig.js
@@ -34,7 +34,7 @@ const config = {
     width: 415,
     height: 575,
     backgroundColor: "#000000",
-    scene: [ BlockPuzzleStart, BlockPuzzle ]
+    scene: [ BlockPuzzleStart, BlockPuzzle, EasyBlockPuzzle ]
 };
 
 const game = new Phaser.Game(config);
--- blockpuzzlestart.js
+++ blockpuzzlestart.js
@@ -9,18 +9,32 @@ preload(){
 
 create(){
    let titleText = this.add.text(140, 200, 'Block Puzzle')
-   let start = this.add.text(170, 230, 'Start')
-   start.setInteractive();
-   start.on('pointerup', () => {
+   let startEasy = this.add.text(170, 230, 'Easy')
+   let startHard = this.add.text(170, 260, 'Hard' )
+   startEasy.setInteractive();
+   startHard.setInteractive();
+   startEasy.on('pointerup', () => {
+    this.scene.stop('BlockPuzzleStart')
+    this.scene.start('EasyBlockPuzzle')
+   })
+   startEasy.on('pointerover', () => {
+    startEasy.setFill('#ff0')
+   })
+   startEasy.on('pointerout', () => {
+    startEasy.setFill('#fff')
+   })
+
+   startHard.on('pointerup', () => {
     this.scene.stop('BlockPuzzleStart')
     this.scene.start('BlockPuzzle')
    })
-   start.on('pointerover', () => {
-    start.setFill('#ff0')
+   startHard.on('pointerover', () => {
+    startHard.setFill('#ff0')
    })
-   start.on('pointerout', () => {
-    start.setFill('#fff')
+   startHard.on('pointerout', () => {
+    startHard.setFill('#fff')
    })
+
 
 }
 
